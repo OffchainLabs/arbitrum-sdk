b76515ddc91c8dec449c0df79a27493b9f1c0ae6
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-05-04 10:35:07 +0100

    Refactor options when creating retryable ticket

--- src/lib/message/L1ToL2MessageCreator.ts
+++ src/lib/message/L1ToL2MessageCreator.ts
@@ -1,14 +1,14 @@
 import { Signer } from '@ethersproject/abstract-signer'
 import { Provider } from '@ethersproject/abstract-provider'
 
-import { L1ToL2MessageGasEstimator } from './L1ToL2MessageGasEstimator'
+import { GasOverrides, L1ToL2MessageGasEstimator } from './L1ToL2MessageGasEstimator'
 import { L1TransactionReceipt } from './L1Transaction'
 import { Inbox__factory } from '../abi/factories/Inbox__factory'
 import { getL2Network } from '../dataEntities/networks'
 import { PayableOverrides } from '@ethersproject/contracts'
 import { BigNumber } from 'ethers'
 import { SignerProviderUtils } from '../dataEntities/signerOrProvider'
-import { MissingProviderArbTsError } from '../dataEntities/errors'
+import { ArbSdkError, MissingProviderArbTsError } from '../dataEntities/errors'
 import { getBaseFee } from '../utils/lib'
 
 /**
@@ -32,22 +32,25 @@ export class L1ToL2MessageCreator {
     options?: {
       excessFeeRefundAddress?: string
       callValueRefundAddress?: string
-    },
-    gasParams?: {
-      maxFeePerGas: BigNumber
-      maxSubmissionFee: BigNumber
-      gasLimit: BigNumber
-      totalL2GasCosts: BigNumber
+      gasEstimationOptions: GasOverrides,
+      gasOverride?: {
+        maxFeePerGas: BigNumber
+        maxSubmissionFee: BigNumber
+        gasLimit: BigNumber
+        totalL2GasCosts: BigNumber
+      },
     },
     overrides: PayableOverrides = {}
   ): Promise<L1TransactionReceipt> {
+    if(options?.gasOverride && options.gasEstimationOptions)
+      throw new ArbSdkError("Cannot set both gasOverride and gasEstimationOptions")
     const sender = await this.l1Signer.getAddress()
     const excessFeeRefundAddress = options?.excessFeeRefundAddress || sender
     const callValueRefundAddress = options?.callValueRefundAddress || sender
     const l1Provider = SignerProviderUtils.getProviderOrThrow(this.l1Signer)
 
     const defaultedGasParams =
-      gasParams ||
+      options?.gasOverride ||
       (await (async () => {
         const gasEstimator = new L1ToL2MessageGasEstimator(l2Provider)
         const baseFee = await getBaseFee(l1Provider)
@@ -59,7 +62,8 @@ export class L1ToL2MessageCreator {
           baseFee,
           excessFeeRefundAddress,
           callValueRefundAddress,
-          l1Provider
+          l1Provider,
+          options?.gasEstimationOptions
         )
       })())
 
