ca446a60eb7616d1f640889158ba936b7b4e72bb
Chris Buckland <cpbuckland88@gmail.com>
2022-05-25 13:58:12 +0200

    Reset timer to 10 seconds

--- waitForNitro.sh
+++ waitForNitro.sh
@@ -3,10 +3,10 @@
 # poll the nitro endpoint until we get a 0 return code
 while true
 do
-	curl -S -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' http://localhost:8547
+	curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' http://localhost:8547
 	if [ "$?" -eq "0" ]; then
 		exit 0
 	else
-		sleep 1
+		sleep 10
 	fi
 done
