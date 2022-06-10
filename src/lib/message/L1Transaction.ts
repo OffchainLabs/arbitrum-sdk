f68676a33a43d08717ac0331f9a648f5eee3d021
Chris Buckland <cpbuckland88@gmail.com>
2022-06-10 14:18:58 +0200

    fix: correct eth deposit aliasing (#65)
    * Added aliasing for EoAs
    
    * Version bump to 3.0.0-beta.1
    
    * Added fromTxReceipt factory method for l1tol2message
    
    * Added comment to parseEthDepositData
    
    * Removed unecessary data substring
    
    * Added link to data decoding
    
    * Used tuple type instead of array
    
    * Move all constructor logic onto eth deposit
    
    * Added fromEventComponents factory method
    
    * Added arb-os comment

--- src/lib/message/L1Transaction.ts
+++ src/lib/message/L1Transaction.ts
@@ -108,6 +108,7 @@ export class L1TransactionReceipt implements TransactionReceipt {
     const messageDeliveredTopic = iface.getEventTopic(
       iface.getEvent('MessageDelivered')
     )
+
     return this.logs
       .filter(log => log.topics[0] === messageDeliveredTopic)
       .map(l => iface.parseLog(l).args as MessageDeliveredEvent['args'])
@@ -215,38 +216,25 @@ export class L1TransactionReceipt implements TransactionReceipt {
     }
   }
 
-  private parseEthDepositData(eventData: string): BigNumber {
-    const parsed = ethers.utils.defaultAbiCoder.decode(
-      ['uint256'],
-      // decode from the first 9 words
-      eventData.substring(0, 64 * 9 + 2)
-    ) as BigNumber[]
-
-    return parsed[0]
-  }
-
   public async getEthDepositMessages(
     l2Provider: Provider
   ): Promise<EthDepositMessage[]> {
-    const chainID = (await l2Provider.getNetwork()).chainId
-
-    return this.getMessageEvents()
-      .filter(
-        e =>
-          e.bridgeMessageEvent.kind ===
-          InboxMessageKind.L1MessageType_ethDeposit
-      )
-      .map(m => {
-        const value = this.parseEthDepositData(m.inboxMessageEvent.data)
-
-        return new EthDepositMessage(
-          l2Provider,
-          chainID,
-          m.inboxMessageEvent.messageNum,
-          m.bridgeMessageEvent.sender,
-          value
+    return Promise.all(
+      this.getMessageEvents()
+        .filter(
+          e =>
+            e.bridgeMessageEvent.kind ===
+            InboxMessageKind.L1MessageType_ethDeposit
         )
-      })
+        .map(m =>
+          EthDepositMessage.fromEventComponents(
+            l2Provider,
+            m.inboxMessageEvent.messageNum,
+            m.bridgeMessageEvent.sender,
+            m.inboxMessageEvent.data
+          )
+        )
+    )
   }
 
   /**
