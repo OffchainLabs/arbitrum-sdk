fbd88f17cb27d4180235baea66a99b5a735f77c1
Chris Buckland <cpbuckland88@gmail.com>
2022-07-28 12:20:53 +0200

    C nitro event log tx hash (#127)
    * Added transaction hash to types
    
    * Updated event logs to add tx hash

--- src/lib/message/L2ToL1Message.ts
+++ src/lib/message/L2ToL1Message.ts
@@ -146,7 +146,7 @@ export class L2ToL1Message {
     position?: BigNumber,
     destination?: string,
     hash?: BigNumber
-  ): Promise<L2ToL1TxEvent['args'][]> {
+  ): Promise<(L2ToL1TxEvent['args'] & { transactionHash: string })[]> {
     const eventFetcher = new EventFetcher(l2Provider)
     return (
       await eventFetcher.getEvents(
@@ -155,7 +155,7 @@ export class L2ToL1Message {
         t => t.filters.L2ToL1Tx(null, destination, hash, position),
         filter
       )
-    ).map(l => l.event)
+    ).map(l => ({ ...l.event, transactionHash: l.transactionHash }))
   }
 }
 
