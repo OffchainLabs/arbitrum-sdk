b8e5c28161cf4e0f4e0b476ef515502677f48839
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-08-08 10:38:07 +0100

    fix classic network pairings to include nitro specific networks

--- src/lib/dataEntities/networks.ts
+++ src/lib/dataEntities/networks.ts
@@ -20,9 +20,15 @@ import dotenv from 'dotenv'
 import { SignerOrProvider } from './signerOrProvider'
 
 import * as classic from '@arbitrum/sdk-classic'
-import { l1Networks as classicl1Networks } from '@arbitrum/sdk-classic/dist/lib/dataEntities/networks'
+import {
+  l1Networks as classicl1Networks,
+  l2Networks as classicL2Networks
+} from '@arbitrum/sdk-classic/dist/lib/dataEntities/networks'
 import * as nitro from '@arbitrum/sdk-nitro'
-import { l1Networks as nitrol1Networks } from '@arbitrum/sdk-nitro/dist/lib/dataEntities/networks'
+import {
+  l1Networks as nitrol1Networks,
+  l2Networks as nitroL2Networks
+} from '@arbitrum/sdk-nitro/dist/lib/dataEntities/networks'
 import {
   convertNetworkClassicToNitro,
   convertNetworkNitroToClassic,
@@ -138,3 +144,13 @@ export const isL1Network = (
   if ((network as L1Network).partnerChainIDs) return true
   else return false
 }
+
+function patchClassicNetworkWithNitro() {
+  // adds nitro specific networks to the classic package to avoid breakage
+  classicl1Networks[1].partnerChainIDs.push(42170)
+  classicl1Networks[5] = nitrol1Networks[5]
+  classicL2Networks[421613] = convertNetworkNitroToClassic(nitroL2Networks[421613])
+  classicL2Networks[42170] = convertNetworkNitroToClassic(nitroL2Networks[42170])
+}
+
+patchClassicNetworkWithNitro();
