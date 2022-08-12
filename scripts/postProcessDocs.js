ec5cc1bce3d1c9f18aad4bba96c829a4245215ee
dzgoldman <dzgoldman@wesleyan.edu>
2022-08-12 18:22:28 -0400

    add postprocessor for sanitizing

---
+++ scripts/postProcessDocs.js
@@ -0,0 +1,29 @@
+const glob = require('glob')
+const { readFile, writeFile } = require('fs')
+
+var getDirectories = function (src, callback) {
+  glob(src + '/**/*', callback)
+}
+getDirectories('./docs', function (err, res) {
+  if (err) {
+    console.log('Error', err)
+  } else {
+    for (const path of res) {
+      if (!path.endsWith('.md')) continue
+      console.log(path)
+
+      readFile(path, 'utf-8', function (err, contents) {
+        if (err) {
+          console.log(err)
+          return
+        }
+        //   Fix JSX md rendering breaking tags:
+        const replaced = contents.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+
+        writeFile(path, replaced, 'utf-8', function (err) {
+          if (err) console.warn('Error', err)
+        })
+      })
+    }
+  }
+})
