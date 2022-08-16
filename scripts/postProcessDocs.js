92f4e95e99e1b231f760f4149435b531790b3002
Daniel Goldman <dzgoldman@wesleyan.edu>
2022-08-16 12:29:14 -0400

    typedoc markdown doc generation   (#148)
    * add typedoc setup
    
    * remove unused files
    
    * add postprocessor for sanitizing
    
    * remove log
    
    * tweak .gitignore
    
    * clean up postprocess script
    
    * exclude readme from postprocessor

---
+++ scripts/postProcessDocs.js
@@ -0,0 +1,23 @@
+const glob = require('glob')
+const { readFile, writeFile } = require('fs')
+
+glob('./docs/**/*', function (err, res) {
+  if (err) {
+    console.log('Error', err)
+  } else {
+    for (const path of res) {
+      if (!path.endsWith('.md') || path.includes('README')) continue
+      readFile(path, 'utf-8', function (err, contents) {
+        if (err) {
+          console.log(err)
+          return
+        }
+        // Fix JSX md rendering breaking tags:
+        const replaced = contents.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+        writeFile(path, replaced, 'utf-8', function (err) {
+          if (err) console.warn('Error', err)
+        })
+      })
+    }
+  }
+})
