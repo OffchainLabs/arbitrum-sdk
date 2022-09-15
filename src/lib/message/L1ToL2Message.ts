035b4dd8ae370278a62eeb8eba8a903b06baa532
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-09-15 22:20:51 +0800

    fix: handle redeem tx not found

--- src/lib/message/L1ToL2Message.ts
+++ src/lib/message/L1ToL2Message.ts
@@ -346,7 +346,7 @@ export class L1ToL2MessageReader extends L1ToL2Message {
             this.l2Provider.getTransactionReceipt(e.event.retryTxHash)
           )
         )
-      ).filter(r => r.status === 1)
+      ).filter(r => isDefined(r) && r.status === 1)
       if (successfulRedeem.length > 1)
         throw new ArbSdkError(
           `Unexpected number of successful redeems. Expected only one redeem for ticket ${this.retryableCreationId}, but found ${successfulRedeem.length}.`
