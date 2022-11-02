05c0de6aaf554086c27e2b3f8c67438127584757
Chris Buckland <cpbuckland88@gmail.com>
2022-11-02 13:24:08 +0100

    Updated comment

--- integration_test/standarderc20.test.ts
+++ integration_test/standarderc20.test.ts
@@ -229,7 +229,7 @@ describe('standard ERC20', () => {
       testState.l2Signer.provider!,
       l2TokenAddr
     )
-    // 3 deposits above - increase this number if more deposit tests added
+    // 5 deposits above - increase this number if more deposit tests added
     const startBalance = depositAmount.mul(5)
     const l2BalanceStart = await l2Token.balanceOf(
       await testState.l2Signer.getAddress()
