d045528fab750d7a5ab7eed6457dca36886d4d3f
Bartek <bartlomiejkramek@outlook.com>
2022-11-07 12:31:21 +0100

    tests for nitro messages

--- integration_test/l1toL2MessageEvents.test.ts
+++ integration_test/l1toL2MessageEvents.test.ts
@@ -1,29 +1,83 @@
-import { testSetup } from '../scripts/testSetup'
 import { L1TransactionReceipt } from '../src'
-import { fundL1, fundL2 } from './testHelpers'
-import { BigNumber, providers } from 'ethers'
-import { parseEther } from '@ethersproject/units'
+import { BigNumber, constants, providers } from 'ethers'
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { assert } from 'chai'
 
-describe('L1toL2Message events', () => {
+describe.only('L1toL2Message events', () => {
   it('does call for nitro events', async () => {
-    const testState = {
-      ...(await testSetup()),
-      l1CustomToken: {} as any,
+    // Receipt from mainnet tx: 0x7cb3395fca076033d84be787ef6e8eba90423a97cd244cc2f2f0e66d8df5d54e
+    const receipt: providers.TransactionReceipt = {
+      to: '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f',
+      from: '0x61A5A09FD00e028F8422033Bee26D6AE2a5dfB65',
+      contractAddress: '',
+      transactionIndex: 290,
+      gasUsed: constants.Zero,
+      logsBloom:
+        '0x00000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000020000400000000000000000000100000000000000000000000000000000080000000000008000000000002400000000000000008000080000000000000000000000000000000020000000000000000000000000000000000000200000000000040000000000000000000000100000000000000008000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000004000',
+      blockHash:
+        '0x5732662f1e9815000f3e0ff7afd8dd1954680ba5fa34379025be4867ab00c721',
+      transactionHash:
+        '0x7cb3395fca076033d84be787ef6e8eba90423a97cd244cc2f2f0e66d8df5d54e',
+      logs: [
+        {
+          transactionIndex: 290,
+          blockNumber: 15439928,
+          transactionHash:
+            '0x7cb3395fca076033d84be787ef6e8eba90423a97cd244cc2f2f0e66d8df5d54e',
+          address: '0x011B6E24FfB0B5f5fCc564cf4183C5BBBc96D515',
+          topics: [
+            '0x23be8e12e420b5da9fb98d8102572f640fb3c11a0085060472dfc0ed194b3cf7',
+            '0x00000000000000000000000000000000000000000000000000000000000b5440',
+            '0x76ec43ceddcd3e094e2b6c7d6557779dc8a5e4e47a1010e4e4bc3a2466354c38',
+          ],
+          data: '0x0000000000000000000000004dbd4fc535ac27206064b68ffcf827b0a60bab3f00000000000000000000000000000000000000000000000000000000000000090000000000000000000000005094a09fd00e028f8422033bee26d6ae2a5dea5495e9b06983c4c8d8e870da858e445738d483a2c2647b3d90cf31c341b74b00bc',
+          logIndex: 493,
+          blockHash:
+            '0x5732662f1e9815000f3e0ff7afd8dd1954680ba5fa34379025be4867ab00c721',
+          removed: false,
+        },
+        {
+          transactionIndex: 290,
+          blockNumber: 15439928,
+          transactionHash:
+            '0x7cb3395fca076033d84be787ef6e8eba90423a97cd244cc2f2f0e66d8df5d54e',
+          address: '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f',
+          topics: [
+            '0xff64905f73a67fb594e0f940a8075a860db489ad991e032f48c81123eb52d60b',
+            '0x00000000000000000000000000000000000000000000000000000000000b5440',
+          ],
+          data: '0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000012000000000000000000000000061a5a09fd00e028f8422033bee26d6ae2a5dfb65000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000071afd498d00000000000000000000000000000000000000000000000000000000000d5c8533ce00000000000000000000000061a5a09fd00e028f8422033bee26d6ae2a5dfb6500000000000000000000000061a5a09fd00e028f8422033bee26d6ae2a5dfb65000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+          logIndex: 494,
+          blockHash:
+            '0x5732662f1e9815000f3e0ff7afd8dd1954680ba5fa34379025be4867ab00c721',
+          removed: false,
+        },
+      ],
+      cumulativeGasUsed: constants.Zero,
+      blockNumber: 15439928,
+      confirmations: 477872,
+      effectiveGasPrice: constants.Zero,
+      byzantium: true,
+      type: 2,
+      status: 1,
     }
 
-    await fundL1(testState.l1Signer)
-    await fundL2(testState.l2Signer)
+    const arbProvider = new JsonRpcProvider('https://arb1.arbitrum.io/rpc')
+    const l1TxnReceipt = new L1TransactionReceipt(receipt)
+    const message = await l1TxnReceipt.getL1ToL2Messages(arbProvider)
 
-    const response = await testState.ethBridger.deposit({
-      amount: parseEther('0.000005'),
-      l1Signer: testState.l1Signer,
-    })
-    const txnReceipt = await response.wait()
-    const l1TxnReceipt = new L1TransactionReceipt(txnReceipt)
-    // TODO: Test the final result for nitro
-    const message = await l1TxnReceipt.getL1ToL2Messages(testState.l2Signer)
+    assert.deepEqual(message, [
+      {
+        messageNumber: BigNumber.from(742464),
+        l2Provider: arbProvider,
+        retryableCreationId:
+          '0xebac787fec7d4f1529da19a72fe58f130373eb6a11bb4300aeadbcce3b709293',
+        autoRedeemId:
+          '0x8b63b138259a173889e60421f246d7428631061b1d050ad27f86084becbeb73b',
+        l2TxHash:
+          '0x64ca81f0ed73c10bb8d08c46e179b58a929dc0ff071fe112471d86d82466774f',
+      },
+    ])
   })
 
   it('does call for classic events', async () => {
@@ -33,7 +87,7 @@ describe('L1toL2Message events', () => {
       from: '0xA928c403Af1993eB309451a3559F8946E7d81F7F',
       contractAddress: '',
       transactionIndex: 0,
-      gasUsed: BigNumber.from(0),
+      gasUsed: constants.Zero,
       logsBloom:
         '0x00000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000000000008000000000002000000000000000008000000000000000000000000000000000000020000000000000000000000000000000000010200000000000000000000000000000800000100000000000000008010001000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000001000000000000000200000000000000000000000000000000000000000800000000000004000',
       blockHash:
@@ -75,10 +129,10 @@ describe('L1toL2Message events', () => {
           removed: false,
         },
       ],
-      cumulativeGasUsed: BigNumber.from(0),
+      cumulativeGasUsed: constants.Zero,
       blockNumber: 14602546,
       confirmations: 1279492,
-      effectiveGasPrice: BigNumber.from(0),
+      effectiveGasPrice: constants.Zero,
       byzantium: true,
       type: 2,
       status: 1,
