0e9c676e8103a9f4007db73d156a563c75ac6b5c
Tuckson <105675159+TucksonDev@users.noreply.github.com>
2023-03-10 15:36:21 +0000

    feat: increase default deposit timeout (#256)

--- src/lib/dataEntities/networks.ts
+++ src/lib/dataEntities/networks.ts
@@ -159,8 +159,10 @@ export const l2Networks: L2Networks = {
     nitroGenesisL1Block: 15447158,
     /**
      * Finalisation on mainnet can be up to 2 epochs = 64 blocks on mainnet
+     * We add 10 minutes for the system to create and redeem the ticket, plus some extra buffer of time
+     * (Total timeout: 30 minutes)
      */
-    depositTimeout: 888000,
+    depositTimeout: 1800000,
   },
   421613: {
     chainID: 421613,
@@ -236,7 +238,12 @@ export const l2Networks: L2Networks = {
     },
     nitroGenesisBlock: 0,
     nitroGenesisL1Block: 0,
-    depositTimeout: 888000,
+    /**
+     * Finalisation on mainnet can be up to 2 epochs = 64 blocks on mainnet
+     * We add 10 minutes for the system to create and redeem the ticket, plus some extra buffer of time
+     * (Total timeout: 30 minutes)
+     */
+    depositTimeout: 1800000,
   },
 }
 
