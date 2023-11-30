b3737b6f991c850b3c1aa1dfa6746494f281d6c8
Doug Lance <doug.lance@gmail.com>
2023-11-30 10:00:26 -0500

    fix lint issue

--- src/lib/dataEntities/networks.ts
+++ src/lib/dataEntities/networks.ts
@@ -354,7 +354,7 @@ const isParentChain = (chain: Chain): chain is ParentChain => {
   return (
     chain &&
     'partnerChainIDs' in chain &&
-    !!chain.partnerChainIDs &&
+    Boolean(chain.partnerChainIDs) &&
     chain.partnerChainIDs.length > 0
   )
 }
