21d04500524a823bacf76c11662db0fd1db70a17
Henry <11198460+godzillaba@users.noreply.github.com>
2025-07-30 10:50:08 -0500

    remove legacy patchwork

--- packages/sdk/scripts/genNetwork.ts
+++ packages/sdk/scripts/genNetwork.ts
@@ -2,14 +2,6 @@ import { loadEnv } from '../src/lib/utils/env'
 import { execSync } from 'child_process'
 import * as fs from 'fs'
 
-import { IERC20Bridge__factory } from '../src/lib/abi/factories/IERC20Bridge__factory'
-import { ethers } from 'ethers'
-import {
-  L2Network,
-  ArbitrumNetwork,
-  mapL2NetworkToArbitrumNetwork,
-} from '../src/lib/dataEntities/networks'
-
 loadEnv()
 
 const isTestingOrbitChains = process.env.ORBIT_TEST === '1'
@@ -35,66 +27,15 @@ function getLocalNetworksFromContainer(which: 'l1l2' | 'l2l3'): any {
   throw new Error('nitro-testnode sequencer not found')
 }
 
-/**
- * the container's files are written by the token bridge deployment step of the test node, which runs a script in token-bridge-contracts.
- * once the script in token-bridge-contracts repo uses an sdk version with the same types and is updated to populate those fields,
- * we can remove this patchwork
- */
-async function patchNetworks(
-  l2Network: L2Network,
-  l3Network: L2Network | undefined,
-  l2Provider: ethers.providers.Provider | undefined
-): Promise<{
-  patchedL2Network: ArbitrumNetwork
-  patchedL3Network?: ArbitrumNetwork
-}> {
-  const patchedL2Network = mapL2NetworkToArbitrumNetwork(l2Network)
-
-  // native token for l3
-  if (l3Network && l2Provider) {
-    const patchedL3Network = mapL2NetworkToArbitrumNetwork(l3Network)
-
-    try {
-      patchedL3Network.nativeToken = await IERC20Bridge__factory.connect(
-        l3Network.ethBridge.bridge,
-        l2Provider
-      ).nativeToken()
-    } catch (e) {
-      // l3 network doesn't have a native token
-    }
-
-    return { patchedL2Network, patchedL3Network }
-  }
-
-  return { patchedL2Network }
-}
-
 async function main() {
   fs.rmSync('localNetwork.json', { force: true })
 
-  let output = getLocalNetworksFromContainer('l1l2')
+  const output = getLocalNetworksFromContainer('l1l2')
 
   if (isTestingOrbitChains) {
     // When running with L3 active, the container calls the L3 network L2 so we rename it here
     const { l2Network: l3Network } = getLocalNetworksFromContainer('l2l3')
-    const { patchedL2Network, patchedL3Network } = await patchNetworks(
-      output.l2Network,
-      l3Network,
-      new ethers.providers.JsonRpcProvider(process.env['ARB_URL'])
-    )
-
-    output = {
-      l2Network: patchedL2Network,
-      l3Network: patchedL3Network,
-    }
-  } else {
-    const { patchedL2Network } = await patchNetworks(
-      output.l2Network,
-      undefined,
-      undefined
-    )
-
-    output.l2Network = patchedL2Network
+    output.l3Network = l3Network
   }
 
   fs.writeFileSync('localNetwork.json', JSON.stringify(output, null, 2))
